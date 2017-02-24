bb91f822c3e7a32d4887f733fc6e78c11f2d2100
xa <xiaesmeamen@me.com>
2017-02-24 10:56:34 -0800

    fail test1 and set up save() and getAll() functions

--- src/Client.php
+++ src/Client.php
@@ -9,6 +9,7 @@ class Client {
         $this->id = $id;
     }
 
+    // getters and setters
     function getName()
     {
         $this->name;
@@ -24,9 +25,29 @@ class Client {
         $this->id;
     }
 
+    // functions
+
     function save()
     {
-        
+        $GLOBALS['DB']->exec("INSERT INTO clients (name) VALUES ('{$this->name}');");
+        $this->id = $GLOBALS['DB']->lastInsertId();
+
+    }
+
+    // static functions below
+
+    static function getAll()
+    {
+        $returned_clients = $GLOBALS['DB']->query("SELECT * FROM clients;");
+        $clients = array();
+        foreach($returned_clients as $client)
+        {
+            $name = $client['name'];
+            $id = $client['id'];
+            $new_client = new Client($name, $id);
+            array_push($clients, $new_client);
+        }
+        return $clients;
     }
 }
  ?>
--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -4,29 +4,31 @@
     * @backupStaticAttributes disabled
     */
 
-    require_once __DIR__."src/Stylist.php";
-    require_once __DIR__."src/Client.php";
+    require_once "src/Stylist.php";
+    require_once "src/Client.php";
 
-    $server = 'mysql:host=localhost:8889;dbname= hair_salon_test';
+    $server = 'mysql:host=localhost:8889;dbname=hair_salon_test';
     $username = 'root';
-    $password = 'password';
+    $password = 'root';
     $DB = new PDO($server, $username, $password);
 
     class ClientTest extends PHPUnit_Framework_TestCase
     {
+        
         function test_save()
         {
             // Arrange
             $name = "Marge Simpleson";
             $id = NULL;
-            $new_client = Client($name, $id);
+            $new_client = new Client($name, $id);
             $new_client->save();
+            var_dump($new_client);
 
             // Act
             $result = Client::getAll();
 
             // Assert
-            $result = ($new_client, $result);
+            $this->assertEquals($new_client, $result[0]);
         }
     }
 
