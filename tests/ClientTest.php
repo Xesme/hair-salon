de33f8c733e3e8df1aa978246f54dbd15b3edafe
xa <xiaesmeamen@me.com>
2017-02-24 15:00:10 -0800

    pass test5 for Client getAll() function

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -40,11 +40,40 @@
 
             // Act
             $result = Client::getAll();
-            // var_dump($result);
+
 
             // Assert
             $this->assertEquals($new_client, $result[0]);
 
+        }
+
+        function test_getAll()
+        {
+            // Arrange
+            $stylist_name = "Beth";
+            $id = NULL;
+            $new_stylist = new Stylist($stylist_name, $id);
+            $new_stylist->save();
+
+
+            $name = "Marge Simpleson";
+            $id3 = NULL;
+            $stylist_id = $new_stylist->getId();
+            $new_client = new Client($name, $id3, $stylist_id);
+            $new_client->save();
+
+            $name2 = "Mary Monroe";
+            $id2 = NULL;
+            $new_client2 = new Client($name2, $id2, $stylist_id);
+            $new_client2->save();
+
+
+            // Act
+            $result = Client::getAll();
+
+
+            // Assert
+            $this->assertEquals([$new_client, $new_client2], $result);
         }
     }
  ?>
