4b7c0d38640e65649b664cbafd4cc2148ee7b0a7
xa <xiaesmeamen@me.com>
2017-03-01 21:33:26 -0800

    fix bug (typo in render for twig)

--- apps/app.php
+++ apps/app.php
@@ -4,7 +4,7 @@
     require_once __DIR__."/../src/Client.php";
     require_once __DIR__."/../src/Stylist.php";
 
-    $server = 'mysql:host=localhost:8889;dbname=hair_salon';
+    $server = 'mysql:host=localhost:8889;dbname=hair-salon';
     $username = 'root';
     $password = 'root';
     $DB = new PDO($server, $username, $password);
@@ -12,12 +12,27 @@
     $app = new Silex\Application();
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
+
     use Symfony\Component\HttpFoundation\Request;
     Request::enableHttpMethodParameterOverride();
 
     $app->get("/", function() use($app){
-        $stylist = Stylist::getAll();
-        return $app['twig']->render('index.html.twig', array('stylist'=> $stylist));
+        $stylists = Stylist::getAll();
+        return $app['twig']->render('index.html.twig', array('stylists' => $stylists));
+    });
+
+    $app->post("/add-stylist", function() use($app){
+        $id = NULL;
+        $new_stylist = new Stylist($_POST['stylist_name'], $id);
+        $new_stylist->save();
+        $stylists = Stylist::getAll();
+        return $app['twig']->render('index.html.twig', array('stylists' => $stylists));
+    });
+
+    $app->get("/stylist/{{id}}", function($id) use($app){
+        $found_client = Client::find($id);
+        $stylists = Stylist::getAll();
+        return $app['twig']->render('stylist.html.twig', array('stylist' => $stylists, 'clients' => $found_client);
     });
 
     return $app;
